4028727e0c9e4cdee8baa639f67fb571e6024772
SoerEighty8 <55634276+SoerEighty8@users.noreply.github.com>
2020-03-13 13:10:57 +0300

    исправления

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
 var link = document.querySelector("section.search-hotel .button-brown");
 var popup = document.querySelector(".mod");
 var form = popup.querySelector("form");
-
+var cheсkin = popup.querySelector("[name=check-in]");
+var chekout = popup.querySelector("[name=check-out]");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.toggle("mod-close");
   popup.classList.add("mod-bounce");
+  cheсkin.focus();
+  popup.classList.remove("mod-error");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!cheсkin.value || !chekout.value) {
+      evt.preventDefault();
+      popup.classList.remove("mod-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("mod-error");
+      console.log("Нужно ввести логин и пароль");
+    }
 });
